3840de1c493e7275f99c92f355f67bc11136cf9f
Liupolar <122888198+Liupolar@users.noreply.github.com>
2023-04-24 11:19:24 +0800

    Create linkedlist.c

---
+++ linkedlist.c
@@ -0,0 +1,17 @@
+#include "linkedlist.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int createNode(Node** top,void num);
+int appendNode(Node** top,void date);
+int Nodelength(Node* top);
+
+int createNode(Node** top,int num){
+  if((*top)==NULL){
+    (*top)=(Node*)malloc(sizeof(Node));
+    if()
+    return 1;
+  }
+  return 0
+}
